9f24636d1e6acc3adfee01286527491599741961
Harsh pandere <83021189+harsh470@users.noreply.github.com>
2024-08-22 05:37:51 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+// let score = "33ab" //"33ab" -> NaN
+// let score = "78"   //"78" -> 78
+// let score = null  
+// let score = undefined
+// let score = true   //true -> 1 ; false -> 0
+// let score = "harh"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNum = Number(score)
+// console.log(typeof valueInNum);
+// console.log(valueInNum)
+
+
+let isLoggedIn = 1   //1 -> true  ; 0 -> false
+// let isLoggedIn = ""   // "" -> false; "harsh" -> true
+let boolIsLoggedIn = Boolean (isLoggedIn)
+// console.log(typeof boolIsLoggedIn)
+// console.log(boolIsLoggedIn)
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
