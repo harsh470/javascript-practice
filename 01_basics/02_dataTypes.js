d1e4873c45d39ea0cb97dd6a87d4a274e4f806a2
Harsh pandere <83021189+harsh470@users.noreply.github.com>
2024-08-22 03:49:41 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+let name = "hites"
+let age = 18
+let isLoggedIn = false
+// Primitive Datatypes
+// number -> 2 to power 53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined -> value is still not assigned
+// symbol -> unique
+
+
+//object
+
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
