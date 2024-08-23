ba0bb6d87877576328af6afd2a46ead754b60280
Harsh pandere <83021189+harsh470@users.noreply.github.com>
2024-08-23 16:20:36 +0000

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive
+//7 types : String, Number, Boolean, null, undefined , Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+// const isLoggedIn = false
+// const outsideTemp = null;
+// console.log(typeof outsideTemp)
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId)
+
+// const bigNumber = 454655656565656565656n
+
+
+
+// Non Primitive (Reference)
+// Array, Objects, Functions
+
+//Array
+// const heros = ["ironmen", "thor", "hulk"] 
+
+//Objects
+// let myObj = {          
+//     name: "harsh",
+//     age: 21,
+// }
+
+//Functions
+// const myFunction = function(){
+//        console.log("Hello World")
+// }
+// myFunction()
+
+// console.log(typeof myFunction)
+
