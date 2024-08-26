ea81913918324f2b3be5c37b60f6f10f91bf6688
Harsh pandere <83021189+harsh470@users.noreply.github.com>
2024-08-26 14:39:36 +0000

    Nums&Math

---
+++ 01_basics/06_nums_&_math.js
@@ -0,0 +1,34 @@
+const score = 900
+
+const balance = new Number(190)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++ Maths ++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // make it positive value
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.1));
+// console.log(Math.min(4,3,8,9));
+// console.log(Math.max(4,3,8,9));
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min)
