07b7aa61088b6979a0029e90aca224442441c042
Harsh pandere <83021189+harsh470@users.noreply.github.com>
2024-09-29 12:53:07 +0000

    function deals with array and objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,3 +37,31 @@ function loginUser(username ="vikas"){
 // console.log(loginUser())
 // console.log(loginUser("harsh"))
 
+
+function calculateCartPrice(val1, val2,...num1){
+    return num1;
+}
+// console.log(calculateCartPrice(200, 300, 400,320))
+
+
+const user ={
+    username : "harsh",
+    price : 993
+}
+function handleObject(anyObject){
+    // console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+handleObject(user)
+//OR use this method 
+// handleObject({
+//     username: "nim",
+//     price: 99
+// })
+
+
+const myNewArray = [200,330, 400, 500]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([200,330,400,500]))
